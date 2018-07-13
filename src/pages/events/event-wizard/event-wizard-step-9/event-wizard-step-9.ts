08d8c1b9e3907048ef029d4508b57bd263141eef
Jakub Sobel <qbasob@gmail.com>
2018-07-13 13:53:48 +0200

    usunięcie niepotrzebnego importi;

--- src/pages/events/event-wizard/event-wizard-step-9/event-wizard-step-9.ts
+++ src/pages/events/event-wizard/event-wizard-step-9/event-wizard-step-9.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { NavController, NavParams, AlertController, App, Events } from 'ionic-angular';
 
 @Component({
