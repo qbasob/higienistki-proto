b745818ad3c4e852b1dbb231619046dce3031e82
Jakub Sobel <qbasob@gmail.com>
2018-07-17 11:56:01 +0200

    dodanie ENV;

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+    mode: 'Development',
+    endpoint: 'http://localhost:3000'
+}
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const ENV = {
+    mode: 'Production',
+    endpoint: 'http://localhost:3000'
+}
