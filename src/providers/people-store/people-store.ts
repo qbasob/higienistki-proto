faf8de3f94f362a4fe9e8f439aec2ab6c611ad64
Lukasz Malec <lukasz.performa@gmail.com>
2018-07-31 00:09:06 +0200

    nic

--- src/providers/people-store/people-store.ts
+++ src/providers/people-store/people-store.ts
@@ -20,7 +20,7 @@ export class PeopleStore extends AbstractStore<Person> {
     http: HttpClient,
     storage: Storage
   ) {
-    const modelName = 'manage/people';
+    const modelName = 'people';
     super(modelName, http, storage);
   }
 }
