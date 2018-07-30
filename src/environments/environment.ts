45e7b430f7405f3e2a9b23c82889323701065d10
Lukasz Malec <lukasz.performa@gmail.com>
2018-07-31 01:28:20 +0200

    drobiazgi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,5 +2,6 @@ export const ENV = {
     mode: 'Production',
     // endpoint: 'http://localhost:3000',
     endpoint: 'http://merch.karlsbad.pl/sonicare',
-    endpointSuffix: '.json'
+    // endpointSuffix: '.json'
+    endpointSuffix: ''
 }
